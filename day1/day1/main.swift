f9e6b90593b2bc0b0c22e194969debadcd9cbcc9
Matt Smollinger <m.smollinger@gmail.com>
2024-12-03 15:17:49 -0500

    feat: integrate input and produce answer

--- day1/day1/main.swift
+++ day1/day1/main.swift
@@ -7,6 +7,30 @@
 
 import Foundation
 
+guard CommandLine.arguments.count > 1 else {
+    print("Please supply file path")
+    exit(1)
+}
+
+print("Reading location ids...")
+guard let data = try? String(contentsOfFile: CommandLine.arguments[1], encoding: .utf8) else {
+    print("Failed to read input data")
+    exit(1)
+}
+
+var list1 = [Int]()
+var list2 = [Int]()
+
+data.enumerateLines(invoking: { line, _ in
+    let locationIds = line.components(separatedBy: "   ")
+    list1.append(Int(locationIds[0])!)
+    list2.append(Int(locationIds[1])!)
+})
+
+print("Calculating distance...")
+let distance = totalDistanceBetweenTwoLists(list1, list2)
+print("Distance between lists: \(distance)")
+
 func totalDistanceBetweenTwoLists(_ list1: [Int], _ list2: [Int]) -> Int {
     var total = 0
     let sortedList1 = list1.sorted(by: { $0 < $1 })
