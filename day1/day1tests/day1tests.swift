c633768a599dc6084052cad5e0818a71f17cacac
Matt Smollinger <m.smollinger@gmail.com>
2024-12-03 14:32:16 -0500

    feat: implement initial algo logic

--- day1/day1tests/day1tests.swift
+++ day1/day1tests/day1tests.swift
@@ -38,6 +38,11 @@ let list2 = [
 
 func totalDistanceBetweenTwoLists(_ list1: [Int], _ list2: [Int]) -> Int {
     var total = 0
+    let sortedList1 = list1.sorted(by: { $0 < $1 })
+    let sortedList2 = list2.sorted(by: { $0 < $1 })
+    for (index1, index2) in zip(sortedList1, sortedList2) {
+        total += abs(index1 - index2)
+    }
     return total
 }
 
