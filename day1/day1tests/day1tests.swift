a4ad702588f3c74b1e0d0e1b2a34eae6814772c1
Matt Smollinger <m.smollinger@gmail.com>
2024-12-03 14:47:15 -0500

    chore: move function into main tool file

--- day1/day1tests/day1tests.swift
+++ day1/day1tests/day1tests.swift
@@ -35,17 +35,6 @@ let list2 = [
     3,
 ]
 
-
-func totalDistanceBetweenTwoLists(_ list1: [Int], _ list2: [Int]) -> Int {
-    var total = 0
-    let sortedList1 = list1.sorted(by: { $0 < $1 })
-    let sortedList2 = list2.sorted(by: { $0 < $1 })
-    for (index1, index2) in zip(sortedList1, sortedList2) {
-        total += abs(index1 - index2)
-    }
-    return total
-}
-
 @Test func testTotalDistanceBetweenTwoLists() {
     #expect(totalDistanceBetweenTwoLists(list1, list2) == 11)
 }
